e0a8818c7f4e7084d15cc07396e293b55e1bc7b0
lkaldyne <l.kaldyne@gmail.com>
2019-06-25 14:13:59 -0400

    Implemented fitment and visualization functionality

--- SVM/implementation/SVM_src.py
+++ SVM/implementation/SVM_src.py
@@ -72,14 +72,59 @@ class Support_Vector_Machine:
 
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+                if w[0] < 0:
+                    optimized = True
+                    print("Optimized a step.")
+                else:
+                    w = w - step
 
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
 
-
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0]+step*2
 
     def predict(self, features):
         # sign(x.w+b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
         return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x . w + b
+        # v = x . w + b
+        # v prospects, usually, 0, 1, -1
+        def hyperplane(x,w,b,v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w . x + b = 1)
+        # Positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1) #First 'y' value for hyperplane
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)  # Second 'y' value for hyperplane
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k') #hyperplane plot
+
+        # (w . x + b = -1)
+        # Negative support vector hyperplane
+        ngf1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        ngf2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [ngf1, ngf2], 'k')
+
+        # (w . x + b = 0)
+        # Decision hyperplane
+        dec1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        dec2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [dec1, dec2], 'y--')
+
+        plt.show()
+
 
 data_dict = {-1:np.array([[1,7],
                           [2,8],
@@ -88,6 +133,8 @@ data_dict = {-1:np.array([[1,7],
                           [6,-1],
                           [7,3]])
              }
-a = Support_Vector_Machine(True)
+clf = Support_Vector_Machine(True)
 
-a.fit(data_dict)
+clf.fit(data=data_dict)
+print (clf.predict([2,5]))
+clf.visualize()
